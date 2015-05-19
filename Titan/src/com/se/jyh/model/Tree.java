a958b5fde53085984617f92ba48b308e7ea5e92d
lgpc <leebduk@gmail.com>
2015-05-20 00:15:40 +0900

    fajfi

--- Titan/src/com/se/jyh/model/Tree.java
+++ Titan/src/com/se/jyh/model/Tree.java
@@ -8,231 +8,239 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.MutableTreeNode;
 import javax.swing.tree.TreePath;
+
 /**
  * 
- * @author lgpc
- * TreeModel
+ * @author lgpc TreeModel
  * 
- * not sure
+ *         not sure
  */
-public class Tree extends JTree  {
+public class Tree extends JTree {
 
-	private static DefaultMutableTreeNode root = new DefaultMutableTreeNode("$root");
-	
+	private static DefaultMutableTreeNode root = new DefaultMutableTreeNode(
+			"$root");
+
 	private int[] index;
-	
+
 	public int[] getIndex() {
 		return index;
 	}
-	
+
 	public void setIndex(int[] index) {
 		this.index = index;
 	}
-	
-	public Tree(){
+
+	public Tree() {
 		super(root);
 	}
-	
-	public int rootSize(){
+
+	public int rootSize() {
 		return root.getChildCount();
 	}
-	
-	public void initNode(DsmModel model){
-		
-		for(int i=0;i<model.getSize();i++){
-			root.add(new DefaultMutableTreeNode(model.getDependencyData_arr()[i].getName()));
+
+	public void initNode(DsmModel model) {
+
+		for (int i = 0; i < model.getSize(); i++) {
+			root.add(new DefaultMutableTreeNode(
+					model.getDependencyData_arr()[i].getName()));
 		}
 		DefaultMutableTreeNode asd = root.getNextNode();
 	}
-	
-	public DefaultMutableTreeNode getRoot(){
+
+	public DefaultMutableTreeNode getRoot() {
 		return root;
 	}
+
 	public void delete() {
 		// TODO Auto-generated method stub
-		DefaultTreeModel model= (DefaultTreeModel) this.getModel();
-		
-		DefaultMutableTreeNode currentNode= root;
-		
-		while(currentNode!=null){
-			if(this.isPathSelected(new TreePath(currentNode.getPath()))){
-				
-				DefaultMutableTreeNode temp=currentNode;
-				currentNode=currentNode.getNextNode();
+		DefaultTreeModel model = (DefaultTreeModel) this.getModel();
+
+		DefaultMutableTreeNode currentNode = root;
+
+		while (currentNode != null) {
+			if (this.isPathSelected(new TreePath(currentNode.getPath()))) {
+
+				DefaultMutableTreeNode temp = currentNode;
+				currentNode = currentNode.getNextNode();
 				model.removeNodeFromParent(temp);
-			}else{
-				currentNode=currentNode.getNextNode();
+			} else {
+				currentNode = currentNode.getNextNode();
 			}
 		}
 	}
-	
+
 	public void expandAll() {
-		for(int i=0;i<this.getRowCount();i++){
+		for (int i = 0; i < this.getRowCount(); i++) {
 			this.expandRow(i);
 		}
-		
+
 	}
 
 	public void collapseAll() {
 		// TODO Auto-generated method stub
-		for(int i=0;i<this.getRowCount();i++){
+		for (int i = 0; i < this.getRowCount(); i++) {
 			this.collapseRow(i);
 		}
 	}
 
 	public void moveUp() {
 		// TODO Auto-generated method stub
-		DefaultTreeModel model= (DefaultTreeModel) this.getModel();
+
+		DefaultTreeModel model = (DefaultTreeModel) this.getModel();
 		List<DefaultMutableTreeNode> selectionList = new ArrayList<DefaultMutableTreeNode>();
-		
+
+		DefaultMutableTreeNode parent = null;
 		DefaultMutableTreeNode selection = null;
-		DefaultMutableTreeNode currentNode= root;
-		
-		while(currentNode!=null){
-			if(this.isPathSelected(new TreePath(currentNode.getPath()))){
-				
-				String beforeName = (String) currentNode.getPreviousSibling().getUserObject();
-				String currentName = (String) currentNode.getUserObject();
-				
-				currentNode.setUserObject(beforeName);
-				currentNode.getPreviousSibling().setUserObject(currentName);
-				
-				model.nodeChanged(currentNode);
-				model.nodeChanged(currentNode.getPreviousSibling());
-				
-				selectionList.add(currentNode.getPreviousSibling());
-				
-				currentNode=currentNode.getNextNode();
-				
-			}else{
-				currentNode=currentNode.getNextNode();
+		DefaultMutableTreeNode currentNode = root;
+
+		while (currentNode != null) {
+			if (this.isPathSelected(new TreePath(currentNode.getPath()))) {
+
+				parent = (DefaultMutableTreeNode) currentNode.getParent();
+				DefaultMutableTreeNode before = currentNode
+						.getPreviousSibling();
+				if(before==null){
+					currentNode =null;
+				}else{
+					DefaultMutableTreeNode temp = currentNode;
+					model.removeNodeFromParent(currentNode);
+					model.insertNodeInto(temp, parent, parent.getIndex(before));
+					selectionList.add(temp);
+				}
+
+			} else {
+				currentNode = currentNode.getNextNode();
 			}
 		}
-		
+
 		TreePath[] selectionPath = new TreePath[selectionList.size()];
-		for(int i=0;i<selectionList.size();i++){
+		for (int i = 0; i < selectionList.size(); i++) {
 			selectionPath[i] = new TreePath(selectionList.get(i).getPath());
 		}
 		this.setSelectionPaths(selectionPath);
 	}
 
 	public void moveDown() {
-		
-		DefaultTreeModel model= (DefaultTreeModel) this.getModel();
+
+
+		DefaultTreeModel model = (DefaultTreeModel) this.getModel();
 		List<DefaultMutableTreeNode> selectionList = new ArrayList<DefaultMutableTreeNode>();
-		
+
+		DefaultMutableTreeNode parent = null;
 		DefaultMutableTreeNode selection = null;
-		DefaultMutableTreeNode currentNode= root;
-		
-		while(currentNode!=null){
-			if(this.isPathSelected(new TreePath(currentNode.getPath()))){
-				
-				String afterName = (String) currentNode.getNextSibling().getUserObject();
-				String currentName = (String) currentNode.getUserObject();
-				
-				currentNode.setUserObject(afterName);
-				currentNode.getNextSibling().setUserObject(currentName);
-				
-				model.nodeChanged(currentNode);
-				model.nodeChanged(currentNode.getNextSibling());
-				
-				selectionList.add(currentNode.getNextSibling());
-				
-				currentNode=currentNode.getNextSibling().getNextSibling();
-				
-			}else{
-				currentNode=currentNode.getNextNode();
+		DefaultMutableTreeNode currentNode = (DefaultMutableTreeNode) root.getLastChild();
+
+		while (currentNode != null) {
+			if (this.isPathSelected(new TreePath(currentNode.getPath()))) {
+
+				parent = (DefaultMutableTreeNode) currentNode.getParent();
+				DefaultMutableTreeNode after = currentNode.getNextSibling();
+				if(after==null){
+					currentNode = null;
+					continue;
+				}else{
+					
+					DefaultMutableTreeNode temp = after;
+					model.removeNodeFromParent(after);
+					model.insertNodeInto(temp, parent, parent.getIndex(currentNode));
+					selectionList.add(currentNode);
+				}
+				currentNode=currentNode.getPreviousSibling();
+
+			} else {
+				currentNode = currentNode.getPreviousNode();
 			}
 		}
-		
+
 		TreePath[] selectionPath = new TreePath[selectionList.size()];
-		for(int i=0;i<selectionList.size();i++){
+		for (int i = 0; i < selectionList.size(); i++) {
 			selectionPath[i] = new TreePath(selectionList.get(i).getPath());
 		}
 		this.setSelectionPaths(selectionPath);
-		
+
 	}
 
 	public void group(String groupName) {
 		// TODO Auto-generated method stub
-		
+
 		List<DefaultMutableTreeNode> selectionList = new ArrayList<DefaultMutableTreeNode>();
 		DefaultMutableTreeNode currentNode = root;
 		DefaultMutableTreeNode parent = null;
-		int index=9999;
+		int index = 9999;
 		DefaultTreeModel model = (DefaultTreeModel) this.getModel();
-		
-		while(currentNode!=null){
-			if(this.isPathSelected(new TreePath(currentNode.getPath()))){
-				if(parent==null){
-					parent=(DefaultMutableTreeNode) currentNode.getParent();
-					index=parent.getIndex(currentNode);
+
+		while (currentNode != null) {
+			if (this.isPathSelected(new TreePath(currentNode.getPath()))) {
+				if (parent == null) {
+					parent = (DefaultMutableTreeNode) currentNode.getParent();
+					index = parent.getIndex(currentNode);
 				}
-				DefaultMutableTreeNode temp=currentNode;
+				DefaultMutableTreeNode temp = currentNode;
 				selectionList.add(currentNode);
-				currentNode=currentNode.getNextNode();
+				currentNode = currentNode.getNextNode();
 				model.removeNodeFromParent(temp);
-			}else{
-				currentNode=currentNode.getNextNode();
+
+			} else {
+				currentNode = currentNode.getNextNode();
 			}
 		}
-		
+
 		DefaultMutableTreeNode newGroup = new DefaultMutableTreeNode(groupName);
-		
+
 		model.insertNodeInto(newGroup, parent, index);
-		
-		for(int i=0;i<selectionList.size();i++){
+
+		for (int i = 0; i < selectionList.size(); i++) {
 			newGroup.add(selectionList.get(i));
 		}
-		
+
 	}
 
 	public void unGroup() {
 		// TODO Auto-generated method stub
-		
+
 		List<DefaultMutableTreeNode> selectionList = new ArrayList<DefaultMutableTreeNode>();
-		
+
 		DefaultMutableTreeNode currentNode = root;
 		DefaultMutableTreeNode parent = null;
-		int index=9999;
+		int index = 9999;
 		DefaultTreeModel model = (DefaultTreeModel) this.getModel();
-		
-		while(currentNode!=null){
-			if(this.isPathSelected(new TreePath(currentNode.getPath()))){
-				
-				
-				if(currentNode.getChildCount()>0){
+
+		while (currentNode != null) {
+			if (this.isPathSelected(new TreePath(currentNode.getPath()))) {
+
+				if (currentNode.getChildCount() > 0) {
 					/**
 					 * ungroup가능
 					 */
-					for(int i=0;i<currentNode.getChildCount();i++){
-						selectionList.add((DefaultMutableTreeNode) currentNode.getChildAt(i));
+					for (int i = 0; i < currentNode.getChildCount(); i++) {
+						selectionList.add((DefaultMutableTreeNode) currentNode
+								.getChildAt(i));
 					}
 					parent = (DefaultMutableTreeNode) currentNode.getParent();
-					index=parent.getIndex(currentNode);
-					
-					DefaultMutableTreeNode temp=currentNode;
-					
-					currentNode=currentNode.getNextNode();
-					
+					index = parent.getIndex(currentNode);
+
+					DefaultMutableTreeNode temp = currentNode;
+
+					currentNode = currentNode.getNextNode();
+
 					model.removeNodeFromParent(temp);
-					
-					for(int i=0;i<selectionList.size();i++){
-						model.insertNodeInto(selectionList.get(i), parent, index+i);
+
+					for (int i = 0; i < selectionList.size(); i++) {
+						model.insertNodeInto(selectionList.get(i), parent,
+								index + i);
 					}
-					
-					
+
 					continue;
-				}else{
+				} else {
 					/**
 					 * 자식없어서 ungroup불가능
 					 */
 					System.out.println("ungroup 안됨");
 				}
-				
-				currentNode=currentNode.getNextNode();
-			}else{
-				currentNode=currentNode.getNextNode();
+
+				currentNode = currentNode.getNextNode();
+			} else {
+				currentNode = currentNode.getNextNode();
 			}
 		}
 	}
